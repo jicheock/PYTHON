8a3d82d88a17fc0502e081a4e5aad68b4e759ab2
jicheock <155503272+jicheock@users.noreply.github.com>
2024-01-03 12:51:51 +0900

    Add files via upload

---
+++ output_input.py
@@ -0,0 +1,15 @@
+# 메모용 주석문 f 문자열 문법  연산이 들어가는 경우--> 중괄호 {}사용
+print("염지석 재밌어 보여서 ㅎㅎ")
+print(f"123+456+789= {123+456+789}")
+print(f"2347890*346827={2347890*346827}")
+# 변수: 값을 담는 상자
+#name 변수 사용
+name= "염지석"
+#= 넣을께
+print(name)
+mbti= "INFJ"
+print(mbti)
+print(f"저의 이름은 {name} mbti는 {mbti}")
+#input  [유저한테 입력값을 담는 기능]
+your_name=input("당신의 이름은:")
+print(f"당신의 이름은 {your_name}이군요")
---
+++ 퀴즈.py
@@ -0,0 +1,17 @@
+# 자기소개
+# 이름 입력
+#mbti 입력
+# 좋아하는 음식
+# 결과 +> 이름이 --이며, mbti --- 지ㅏㅓㄴㅇㅁ허
+
+name=("염지석")
+mbti="INFJ"
+food=("potato")
+name=input("나의 이름은:")
+mbti=input("mbti 입력:")
+food=input("food입력:")
+print(f"이름은 {name} mbti는 {mbti} food는 {food}이군요! ")
+movie= input("최근에 본 영화")
+ott=input("본 거")
+print(f"최근에 본 영화 {movie} ott{ott}")
+print(ㄹ)
