ea9c8b3ac8a19429ae78f585c599774c2bd2e658
jicheock <155503272+jicheock@users.noreply.github.com>
2024-01-04 12:49:44 +0900

    Add files via upload

---
+++ day 02/data file..py
@@ -0,0 +1,15 @@
+first=5
+second=7
+num = int(5)
+num= int(7)
+print(f"합: {first+second}")
+third=3.14
+num=float(3.14)
+num=float(second)
+print(f"합: {third*second}")
+#input(): 유저한테 입력받는 기능
+#print(): 안에 들어있는 내용 출력
+#int(): 안에 들어있는 문자를 정수화
+#a=숫자 정수형 타입
+#b= 숫자 실수형타입
+#c= 문자형 타입
---
+++ operator.py
@@ -0,0 +1,14 @@
+#operator
+#숫자 사칙 연산자
+# +-*/
+#문자 연산자
+#1+2=12
+#print(), input, int, str, float,
+# type: 타입 알려주는 기능
+print(type('123'))
+#/(나눗셈 연산자, %(나머지 연산자)
+print(5/2)
+print(5%2)
+# //(정수 나머지 몫) **제곱연산자
+print(5//2)
+print(2**19)
